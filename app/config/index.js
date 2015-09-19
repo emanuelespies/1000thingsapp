243fea2faef94f734fb8c8b537aa4118cbd228b0
Emanuele Spies <emanuelespies@gmail.com>
2015-09-19 18:27:17 -0300

    not sure if it is the right way to connect to mongo

--- app/config/index.js
+++ app/config/index.js
@@ -10,10 +10,11 @@ var config = {
 		mode: 'production',
 		port: 5000,
 		mongo: {
-			url: "mongodb://espies:heroku_gqt2nnf1@ds051553.mongolab.com:51553/heroku_gqt2nnf1"
+			url: "mongodb://heroku_gqt2nnf1:nibqa6n4t0m4762e96943j44l4@ds051553.mongolab.com:51553/heroku_gqt2nnf1"
 		},
 	}
 }
 module.exports = function(mode) {
 	return config[mode || process.argv[2] || 'local'] || config.local;
 }
+
